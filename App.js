5c86fd0174905da30167298e1fb07170702c9388
joseph.stallone@dreamorbit.com <joseph.stallone@dreamorbit.com>
2022-06-23 11:45:11 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { FlatList } from "react-native-gesture-handler";
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  const handlePress1 = () => {
+    navigation.navigate("Details1");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <ScrollView>
+        <TouchableOpacity onPress={handlePress1}>
+          <Text style={styles.item}>item1</Text>
+          <Text style={styles.item}>item2</Text>
+          <Text style={styles.item}>item3</Text>
+          <Text style={styles.item}>item4</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+}
+
+function DetailsScreen1() {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <View>
+        <Image
+          style={{ height: 300, width: 300, marginTop: 30 }}
+          source={require("./assets/image-stand-out.jpg")}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  item: {
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    width:300,
   },
 });
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details1" component={DetailsScreen1} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
